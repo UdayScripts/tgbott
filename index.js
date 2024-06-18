7880cd326bc3752971455ddfae56ca4d1c797d7f
Uday <110768804+UdayScripts@users.noreply.github.com>
2024-06-18 11:34:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ const { promisify } = require('util');
 const moment = require('moment-timezone');
 
 
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running!');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
 
 // Replace with your bot token from @BotFather using environment variables for security
 const token = process.env.TELEGRAM_BOT_TOKEN || '6368266674:AAF8BKgaVNdR7guyACnsQH3FpQHbjl60bhE';
